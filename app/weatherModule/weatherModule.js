8d7301f733c9c2ccb064fde330e0fc6e5d73dd41
mistabenjo <benakehurst@hotmail.com>
2017-11-07 23:51:19 +0200

    logic/markup/styling for tel aviv

--- app/weatherModule/weatherModule.js
+++ app/weatherModule/weatherModule.js
@@ -6,6 +6,23 @@
 
     weatherModule.controller("WeatherController", function($http, $scope, $rootScope, $location) {
 
+        var weatherResults = [];
+        $scope.weatherResults = weatherResults;
+
+        $scope.init = function(){
+            $http({
+                method: "GET",
+                url: '/scrapeTlv'
+            }).then(function success(response) {
+                weatherResults.push(response.data);
+            }, function error(response) {
+                console.log(response.statusText);
+            });
+        };
+        $scope.init();
+
+        console.log(weatherResults);
+
     });
 
 })();
